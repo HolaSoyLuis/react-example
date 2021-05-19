0300c791f6fa208cb20aaeb4349157f971cb138f
HolaSoyLuis <luis95velasquez@outlook.com>
2021-05-18 19:33:19 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0)
+    const [text, setText] = useState('')
+    const [list, setList] = useState([])
+
+    useEffect(() => {
+        setList([...list, 'hi'])
+    }, [])
+
+    const handleText = e => {
+        setText(e.target.value)
+    }
+
+    const addItem = e => {
+        e.preventDefault()
+        console.log('New item: ', text)
+        setList([...list, text])
+        setText('')
+    }
+
+    const clicked = () => {
+        setCount(count + 1)
+    }
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={clicked}>Click me!</button>
+            <p>Add item to the list -> Form</p>
+            <form onSubmit={addItem}>
+                <label htmlFor="inputText">Texto:</label>
+                <input type="text" name="inputText" id="inputText" value={text} onChange={handleText} />
+                <button type="submit">Add :D</button>
+            </form>
+            <p>Items list:</p>
+            <ol>
+                {/*showList*/}
+                {list.map(item => <li key={item}>{item}</li>)}
+            </ol>
+        </div>
+    )
 }
 
-export default App;
+export default App
